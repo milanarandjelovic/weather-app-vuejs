353c9475dd96c4a92eaa790d94644df7bc0255b9
Milan Arandjelovic <milanarandjelovic@users.noreply.github.com>
2016-10-02 12:59:04 +0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -8,10 +8,6 @@ elixir(mix => {
   mix.sass('app.scss')
     .copy('node_modules/font-awesome/fonts/', 'public/fonts')
     .browserify('app.js')
-    /**
-     * The prefix ! is for excluding files.
-     * Excluding file  'Laravel' is not defined  no-undef and  'Vue' is not defined  no-undef
-     */
     .eslint([
       'resources/assets/js/**/*.js',
       'resources/assets/js/**/*.vue'
